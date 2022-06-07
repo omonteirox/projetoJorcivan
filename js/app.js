1b89a5c9880f7b6d06b28f4bc28934af6980da62
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-06-07 14:09:12 -0300

    materialize

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// $( document ).ready(()=>{
+//   $("#menu").load("nav.html");
+//     $(".dropdown-trigger").dropdown();
+// })
 $(function(){
     $("#menu").load("nav.html");
+    $(".dropdown-trigger").dropdown();
   });
