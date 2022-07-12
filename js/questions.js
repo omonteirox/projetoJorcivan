eecbe581e89832f860a162e357a57c9a0c439d84
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-07-12 09:20:10 -0300

    Pronto

---
+++ js/questions.js
@@ -0,0 +1,39 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Dinossauros são?",
+    answer: "Legais Demais!!",
+    options: ["Legais Demais!!", "Ah, ok", "Chatos", "Insuportáveis"],
+  },
+  {
+    numb: 2,
+    question: "Qual dinossauro é conhecido como o rei?",
+    answer: "T-REX",
+    options: ["Não sei", "Pterodátilo", "Brontossauro", "T-REX"],
+  },
+  {
+    numb: 3,
+    question: "teste",
+    answer: "Resposta Certa",
+    options: ["Resposta Certa", "teste", "teste", "teste"],
+  },
+  {
+    numb: 4,
+    question: "teste",
+    answer: "Resposta Certa",
+    options: ["Resposta Certa", "teste", "teste", "teste"],
+  },
+  {
+    numb: 5,
+    question: "teste",
+    answer: "Resposta Certa",
+    options: ["Resposta Certa", "teste", "teste", "teste"],
+  },
+  {
+    numb: 63,
+    question: "teste",
+    answer: "Resposta Certa",
+    options: ["Resposta Certa", "teste", "teste", "teste"],
+  },
+];
